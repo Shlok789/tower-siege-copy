df3739a87dede64e0d6b1ef0d54ddf6af52a4fb2
Shlok789 <66046111+Shlok789@users.noreply.github.com>
2020-08-06 20:46:07 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# tower-siege-copy
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,17 @@
+
+
+
+
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+
+
+  ground = new Ground(600,height,1200,20);
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+  ground.display();
+}
